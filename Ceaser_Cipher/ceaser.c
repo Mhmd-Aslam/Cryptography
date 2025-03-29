abc723f0c0793ae52096dd0738ad92083ed0ad4e
Mhmd-Aslam <aslamaass108@gmail.com>
2025-03-29 10:58:16 +0530

    Ceaser_Cipher

---
+++ Ceaser_Cipher/ceaser.c
@@ -0,0 +1,65 @@
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+int main()
+{
+	char ea[100],eb[100],da[100],db[100],key=3;
+	int n,i,option;
+	printf("Enter Option(1 or 2):\t1.Encrypt\t2.Decrypt :\n");
+	scanf("%d",&option);
+	getchar();
+	switch(option)
+	{
+		case 1:
+		printf("Enter The Message to Encrypt(without spaces) :");	
+		scanf("%s",ea);
+		n=strlen(ea);
+		for (i = 0; i < n; i++) 
+		{
+		    ea[i] = toupper(ea[i]);
+		}
+		
+		for (i = 0; i < n; i++) 
+		{
+    		eb[i] = ((ea[i] - 'A' + key) % 26 + 26) % 26 + 'A';  
+		}
+
+		
+		printf("Cipher Text :");
+		for (i = 0; i < n; i++) 
+		{
+			printf("%c",eb[i]); 
+		}
+		break;
+		printf("\n");
+		
+		case 2:
+		printf("Enter Cipher Text (without spaces):");
+		scanf("%s",da);
+		n=strlen(da);
+		
+		for (i = 0; i < n; i++) 
+		{
+		    da[i] = toupper(da[i]);
+		}
+		
+		for (i = 0; i < n; i++) 
+		{
+			db[i] = ((da[i] - 'A' - key + 26) % 26 + 26) % 26 + 'A';   
+		}
+		
+		printf("Plain Text :");
+		for (i = 0; i < n; i++) 
+		{
+			printf("%c",db[i]); 
+		}
+		break;
+		
+		default:
+        printf("Invalid Option, Enter 1 or 2");
+        break;
+		
+	}
+	printf("\n");
+	return 0;
+}
